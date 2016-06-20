ca7f1ca1dee506e034ef02f12e43c2929c2975ae
Rob Burgertech <Burger@bogen.local>
2016-06-20 14:14:18 +0200

    created new module with new view for contacts

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+	'name' : 'Contact App v1.0.0',
+	'description' : 'This app will display contact info for a prospect',
+	'author' : 'Rob Burger',
+	'depends' : 'website',
+	'summary' : 'Template',
+	'website' : 'www.github.com',
+	#'data' : []
+}
